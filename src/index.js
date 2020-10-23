dfcdee05783397185da5ecbe4989be7d51a5009b
cekip88 <cekip@mail.ru>
2020-10-23 16:48:16 +0600

    complete

--- src/index.js
+++ src/index.js
@@ -38,9 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let words = [];
+    for (let i = 0; i < expr.length; i += 10){
+        let word = expr.substr(i,10);
+        let symbol = '';
+        for (let j = 0; j < word.length; j += 2){
+            if(word[j] !== '0'){
+                 symbol += word[j] + word[j + 1]
+            }
+        }
+        words.push(symbol);
+    }
+
+    let encodeWords = [];
+    words.forEach(function (el) {
+        let symbol = '';
+        for (let i = 0; i < el.length; i += 2){
+            if(el[i] + el[i + 1] === '10') symbol += '.';
+            else if(el[i] + el[i + 1] === '11') symbol += '-';
+            else {
+                symbol = ' ';
+                break;
+            }
+        }
+        encodeWords.push(symbol);
+    });
+
+    let resultStr = '';
+    encodeWords.forEach(function (el) {
+        if(el !== ' ') resultStr += MORSE_TABLE[`${el}`];
+        else resultStr += ' ';
+    });
+
+    return resultStr;
 }
 
 module.exports = {
     decode
-}
+};
